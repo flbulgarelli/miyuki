694034eb9f65ec4bf66111ef30eef5edf29bca06
Diana López Alvas <dlopezalvas@gmail.com>
2025-03-25 01:28:19 -0300

    Using DB_FILE_ID from .env

--- packager/scripts/getDockerComposeFiles.sh
+++ packager/scripts/getDockerComposeFiles.sh
@@ -10,11 +10,6 @@ else
   exit 1
 fi
 
-if [ -z "$DIST_NAME" ]; then
-  echo "DIST_NAME is not set in .env file"
-  exit 1
-fi
-
 if [ ! -d "$DIR" ]; then
   mkdir "$DIR"
 fi
@@ -26,14 +21,8 @@ cp ../dists/$DIST_NAME/docker-compose.yml "$DIR/docker-compose.$DIST_NAME.yml" |
 echo "Getting database..."
 
 DB_DIR="$DIR/development.sqlite3"
-DB_FILE_ID="1ecb0zk7EDY-71P_H-2lvX26DrsWGLOrj"
 if [ ! -f "$DB_DIR" ]; then
   curl -L -o "$DB_DIR" "https://drive.google.com/uc?export=download&id=$DB_FILE_ID" || { echo "Failed to download development.sqlite3"; exit 1; }
 else
   echo "development.sqlite3 already exists, skipping download"
 fi
-
-
-
-
-
