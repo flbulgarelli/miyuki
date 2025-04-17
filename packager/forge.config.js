43401f6f00c010c4fb148a9af7b69384c6f12cf6
Diana Lopez Alvas <dlopezalvas@gmail.com>
2025-04-17 17:38:29 -0300

    adding icon

--- packager/forge.config.js
+++ packager/forge.config.js
@@ -1,18 +1,26 @@
+const path = require('path');
+
 module.exports = {
   packagerConfig: {
     asar: false,
     extraResource: ['docker/'],
-    executableName: "Miyuki"
+    executableName: "Miyuki",
+    icon: path.resolve(__dirname, 'src/favicon')
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+      name: '@electron-forge/maker-squirrel', 
+      config: {
+        iconUrl: 'file://' + path.resolve(__dirname, 'src/favicon.ico'),
+        setupIcon: path.resolve(__dirname, 'src/favicon.ico'),
+      },
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        icon: path.resolve(__dirname, 'src/favicon.png')
+      },
     },
   ],
 };
