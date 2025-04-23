0de95be1f77a8915c590f223188dc74ad4608bf5
Franco Leonardo Bulgarelli <franco@mumuki.org>
2025-04-23 00:27:10 -0300

    Move about text to i18n

--- app/helpers/about_us_helper.rb
+++ app/helpers/about_us_helper.rb
@@ -3,16 +3,7 @@ module AboutUsHelper
     <<~HTML.html_safe
     <section class="exercise-assignment">
       <blockquote>
-        <p>
-          👋 ¡Hola! ¿Sabías que <a href="https://github.com/flbulgarelli/miyuki"><i class="fa fa-solid fa-snowflake"></i> Miyuki</a> es un proyecto colaborativo y de código abierto? Gracias a Miyuki podés instalar y ejecutar un laboratorio de <a href="https://github.com/mumuki/mumuki-laboratory"><i class="da da-mumuki"></i> Mumuki</a> en tu computadora.
-        </p>
-        <p>
-          🌟 Si estás aprendiendo con este software, te pedimos que le des una estrella a estos proyectos y sigas
-          sus novedades.
-        </p>
-        <p>
-          💬 También te esperamos en el canal de <a href="https://discord.gg/Ak4ee7fcSM">Discord de Miyuki</a>.
-        </p>
+        #{t :about_miyuki}
       </blockquote>
     </section>
     HTML
