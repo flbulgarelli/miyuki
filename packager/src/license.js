3bff76789e53c5f371300d51c556ca2dd3c201e5
Diana Lopez Alvas <dlopezalvas@gmail.com>
2025-04-24 16:29:58 -0300

    fixing logic repetition

--- packager/src/license.js
+++ packager/src/license.js
@@ -2,15 +2,14 @@ const { app } = require('electron');
 const fs = require('fs');
 const path = require('path');
 
+const userDataPath = app.getPath('userData');
+const licensePath = path.join(userDataPath, 'license-accepted');
+
 function hasAcceptedLicense() {
-    const userDataPath = app.getPath('userData');
-    const licensePath = path.join(userDataPath, 'license-accepted');
     return fs.existsSync(licensePath);
 }
 
 function saveLicenseAcceptance() {
-    const userDataPath = app.getPath('userData');
-    const licensePath = path.join(userDataPath, 'license-accepted');
     fs.writeFileSync(licensePath, 'accepted');
 }
 
